5511614a5d60bdebd252c2e917bcba51ddfb27d6
Paul <paulpaul81@googlemail.com>
2020-09-23 19:31:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('.social-media');
+let icons = document.querySelector('.icons')
+
+button.addEventListener('click', function(){
+  icons.classList.toggle('hidden');
+});
